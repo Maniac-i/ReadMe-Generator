5d070983d2714aaa32caa026dffb1ec6568caf6b
Joe Maniaci <joe.maniaci1@gmail.com>
2020-12-13 15:23:46 -0500

    updated generateMarkdown function with a markdown layout. added in users data.

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -2,6 +2,48 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+[![Generic badge](https://img.shields.io/badge/license-${data.license}-green.svg)](https://shields.io/)
+
+
+**Table of Contents**
+ - [Description](Description)
+ - [Installation](Installation)
+ - [Usage](Usage)
+ - [License](License)
+ - [Contributing](Contributing)
+ - [Tests](Tests)
+ - [Questions](Questions)
+---
+## Description
+${data.description}
+
+---
+## Installation
+${data.install}
+---
+## Usage
+${data.usageInfo}
+---
+## License
+This application is covered under the ${data.license} license.
+---
+## Contributing
+${data.contribution}
+---
+## Tests
+${data.test}
+---
+## Questions 
+
+**GitHub Username:** ${data.gitHub}
+
+**GitHub URL:** github.com/${data.gitHub}
+
+For any additional questions you can contact me at *${data.email}*.
+
+
+
+
 `;
 }
 
