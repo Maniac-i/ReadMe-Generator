0f68eb8e7b3792facf86173a2db1b105e3cb72c4
Joe Maniaci <joe.maniaci1@gmail.com>
2020-12-13 14:12:00 -0500

    added questions about the ReadMe that get displayed to the user

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 // array of questions for user
 const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your projects title?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description of the project.',
+  },
+  {
+    type: 'input',
+    name: 'install',
+    message: 'Enter installation instructions.',
+  },
+  {
+    type: 'input',
+    name: 'usageInfo',
+    message: 'Enter usage information.',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'Enter contribution guidelines.',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'Enter test instructions.',
+  },
+  {
+    type: 'input',
+    name: 'gitHub',
+    message: 'Enter your GitHub username.',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email.',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license.',
+    choices: ["Public Domain", "Permissive", "LGPL", "Copyleft", "Proprietary"],
+  },
 
 ];
 
